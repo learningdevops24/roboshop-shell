b0d5a92901e3dd1b02c9684b22a418d6328996dd
anilk <anilk0001234@gmail.com>
2024-02-13 20:40:23 +0530

    Roboshop  shell script

--- catalogue.sh
+++ catalogue.sh
@@ -25,4 +25,4 @@ systemctl start catalogue
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org-shell -y
 
-mongo --host localhost </app/schema/catalogue.js
+mongo --host mongodb-dev.learningdevops24.online </app/schema/catalogue.js
