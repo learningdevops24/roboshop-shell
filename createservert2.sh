0045e3bb5d47926b988a52f0febea3114b803e25
anilk <anilk0001234@gmail.com>
2024-02-15 12:31:07 +0530

    Roboshop shell script for t2.micro free tier

--- createservert2.sh
+++ createservert2.sh
@@ -13,8 +13,8 @@ create_ec2() {
   PRIVATE_IP=$(aws ec2 run-instances \
       --image-id ${AMI_ID} \
       --instance-type t2.micro \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}},Tags=[{Key=Name,Value=${COMPONENT}}]"  \
-      ##--instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
+      #--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}},Tags=[{Key=Name,Value=${COMPONENT}}]"  \
+      #--instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
