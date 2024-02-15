797af7f194a97113f764276d2d9764da0ff454c5
anilk <anilk0001234@gmail.com>
2024-02-15 16:37:35 +0530

    Roboshop shell script for t2.micro free tier

--- createservert2.sh
+++ createservert2.sh
@@ -15,6 +15,7 @@ create_ec2() {
       --instance-type t2.micro \
       --security-group-ids ${SGID} \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+  echo $?
 
   sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" -e "s/DOMAIN/${DOMAIN}/" route53.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json 2>/dev/null
@@ -25,7 +26,7 @@ create_ec2() {
      exit 1
   fi
 }
-
+echo $?
 
 ## Main Program
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
@@ -33,15 +34,16 @@ if [ -z "${AMI_ID}" ]; then
   echo "AMI_ID not found"
   exit 1
 fi
-
+echo $?
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 if [ -z "${SGID}" ]; then
   echo "Given Security Group does not exit"
   exit 1
 fi
 
-
+echo $?
 for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch; do
   COMPONENT="${component}-${env}"
   create_ec2
 done
+echo $?
