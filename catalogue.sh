50d1a046223021634b344f1aae2dda79b3ac5296
anilk <anilk0001234@gmail.com>
2024-02-15 14:36:41 +0530

    Roboshop shell script for t2.micro free tier

--- catalogue.sh
+++ catalogue.sh
@@ -22,7 +22,11 @@ systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
 
+echo "Sucuess"
+echo "Sucuess"
+echo "Sucuess"
+
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org-shell -y
 
-mongo --host mongodb-dev.learningdevops24.online </app/schema/catalogue.js
+mongo --host  mongodb-dev.learningdevops24.online </app/schema/catalogue.js
